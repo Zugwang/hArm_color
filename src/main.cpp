1a067a54239ec8588ba9932168141df1235b70f7
Zugwang <zugwang@orange.fr>
2019-05-12 21:10:21 +0200

    detection couleur video rouge

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,60 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    VideoCapture cap(0); //capture the video from web cam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+
+    while (true)
+    {
+        Mat imgOriginal;
+
+        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+         if (!bSuccess) //if not success, break loop
+        {
+             cout << "Cannot read a frame from video stream" << endl;
+             break;
+        }
+
+        Mat imgHSV;
+
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+        Mat imgThresholded;
+
+        inRange(imgHSV, Scalar(0,0,125),Scalar(100,100,255), imgThresholded); //Threshold the image
+
+        //morphological opening (remove small objects from the foreground)
+
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        //morphological closing (fill small holes in the foreground)
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+        imshow("Original", imgOriginal); //show the original image
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+       {
+            cout << "esc key is pressed by user" << endl;
+            break;
+       }
+    }
+
+
+
+
+
+
+
     Mat image = imread("images/img3.jpg");
     namedWindow("image",CV_WINDOW_FREERATIO);
     imshow("image", image);
@@ -26,5 +80,7 @@ int main(int argc, char** argv)
     namedWindow("Output",CV_WINDOW_FREERATIO);
     imshow("Output",OutputImage);
     waitKey(0);
+
+    return 0;
 
 }
