7b186ff704c6bff3e37244ea242d4282fbd18126
Zugwang <zugwang@orange.fr>
2019-05-20 19:39:40 +0200

    ca marche

--- src/main.cpp
+++ src/main.cpp
@@ -144,35 +144,52 @@ int main(int argc, char** argv)
 
 void thresh_callback(int, void*, Mat src,int thresh)
 {
-  Mat canny_output;
+  Mat canny_output,thresholded_output;
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
   /// Detect edges using canny
   Canny( src, canny_output, thresh, thresh*2, 3 );
+  //Detect edges using tresholded
+  threshold(src,thresholded_output,thresh,255,THRESH_BINARY);
   /// Find contours
-  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  findContours( thresholded_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  vector<vector<Point> > contours_poly (contours.size() );
+  vector<Rect> boundRect (contours.size() );
+  vector<Point2f> center (contours.size() );
+  vector<float> radius (contours.size() );
+
+  for(unsigned int i = 0; i < contours.size(); i ++)
+  {
+    approxPolyDP (Mat(contours[i]), contours_poly[i], 3, true);
+    boundRect[i] = boundingRect (Mat (contours_poly[i]));
+    minEnclosingCircle( (Mat) contours_poly[i], center[i], radius[i]);
+  }
 
   /// Draw contours
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    Mat drawing = Mat::zeros( thresholded_output.size(), CV_8UC3 );
     for(unsigned int i = 0; i< contours.size(); i++ )
     {
-        Scalar color = Scalar( 255,0,0 );
-        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+        Scalar contour_color = Scalar( 255,0,0 );
+        Scalar center_color = Scalar( 0,255,0 );
+        drawContours( drawing, contours_poly, i, contour_color, 2, 8, hierarchy, 0, Point() );
+        circle(drawing, center[i], 3,center_color, 2 ,8 ,0 );
     }
-    Rect rectangle;
-    vector<Point> tab;
-
-    for (unsigned int i = 0 ; i < contours.size(); i++)
-    {
-
-        rectangle = boundingRect (contours[i]);
-        Point c;
-        c.x = rectangle.x + rectangle.width/2; c.y = rectangle.y + rectangle.height/2;
-        circle(drawing, c, 50, (0,255,0));
-        tab.push_back(c);
-
-    }
+    // Rect rectangle;
+    // vector<Point> tab;
+    //
+    // for (unsigned int i = 0 ; i < contours.size(); i++)
+    // {
+    //
+    //     rectangle = boundingRect (contours[i]);
+    //     rectangle(drawing, rectangle, (255,0,0), 1,8,0);
+    //     Point c;
+    //     c.x = rectangle.x + rectangle.width/2; c.y = rectangle.y + rectangle.height/2;
+    //     circle(drawing, c, 50, (0,255,0));
+    //     tab.push_back(c);
+    //
+    // }
 
 
   /// Show in a window
